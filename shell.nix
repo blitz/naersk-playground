f655853f04737a3c8554aa712832209fa2c046ac
Julian Stecklina <js@alien8.de>
2021-06-24 21:55:31 +0200

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ sources ? import ./nix/sources.nix
+, nixpkgs ? sources.nixpkgs
+, pkgs ? import sources.nixpkgs {
+  overlays = [
+    (import sources.rust-overlay)
+  ];
+}
+}:
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.niv
+    pkgs.rust-bin.stable.latest.default
+  ];
+}
